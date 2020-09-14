70e6b4f46d1f9d8d452a1ff355a294c9243e28d2
RMeyerhofer <ricard.meyerhofer@est.fib.upc.edu>
2020-09-14 08:46:29 +0200

    minor changes

--- wayfarer-map/ui.R
+++ wayfarer-map/ui.R
@@ -7,26 +7,7 @@ shinyUI(
       tags$link(href = "https://fonts.googleapis.com/css?family=Oswald", rel = "stylesheet"),
       tags$style(type = "text/css", "html, body {width:100%;height:100%; font-family: Oswald, sans-serif;}"),
       tags$script(src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.16/iframeResizer.contentWindow.min.js",
-                  type="text/javascript"),
-      tags$script('
-                  $(document).ready(function () {
-                    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-                    
-                    function onError (err) {
-                      Shiny.onInputChange("geolocation", false);
-                    }
-                    
-                    function onSuccess (position) {
-                      setTimeout(function () {
-                        var coords = position.coords;
-                        console.log(coords.latitude + ", " + coords.longitude);
-                        Shiny.onInputChange("geolocation", true);
-                        Shiny.onInputChange("lat", coords.latitude);
-                        Shiny.onInputChange("long", coords.longitude);
-                      }, 1100)
-                    }
-                  });
-      ')
+                  type="text/javascript")
     ),
     
     leafletOutput("map", width = "100%", height = "100%"),
@@ -34,12 +15,15 @@ shinyUI(
     absolutePanel(
       top = 10, right = 10, style = "z-index:500; text-align: right;",
       tags$h2("My Wayspots"),
-      tags$a("Repository", href="https://github.com/Dirken/wayfarer-nominations-map")
+      tags$a("Based on Crime-Watch", href="https://github.com/PaulC91/crime-watch"),
+      br(),
+      tags$a("Code repository", href="https://github.com/Dirken/wayfarer-nominations-map"),
+      br(),
+      tags$a("Wayfarer+", href="https://github.com/MrJPGames/WayFarerPlus")
     ),
     
     absolutePanel(
       top = 100, left = 10, draggable = TRUE, width = "20%", style = "z-index:500; min-width: 300px;",
-      checkboxInput("use_location", "Or use your current location?"),
       fileInput("file", "Browse file")
       #highchartOutput("selectstat")
     )
